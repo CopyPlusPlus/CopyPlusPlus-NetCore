0577a3011ca54560744999366ae38e357401cd1b
wy-luke <li1078797083@qq.com>
2021-03-21 22:29:36 +0800

    The first version of this app.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WK.Libraries.SharpClipboardNS;
 
 namespace CopyPlusPlus
 {
@@ -23,6 +25,25 @@ namespace CopyPlusPlus
         public MainWindow()
         {
             InitializeComponent();
+
+            var clipboard = new SharpClipboard();
+            // Attach your code to the ClipboardChanged event to listen to cuts/copies.
+            clipboard.ClipboardChanged += ClipboardChanged;
+        }
+
+        private void ClipboardChanged(Object sender, SharpClipboard.ClipboardChangedEventArgs e)
+        {
+            string text = e.Content.ToString();
+            string text_after = text.Replace("\n","").Replace("\r","");
+            Clipboard.SetText(text_after);
+
+            Debug.WriteLine(text);
+            Debug.WriteLine(text_after);
+        }
+
+        private void Todolist_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("你是想帮我完成这个功能吗?");
         }
     }
 }
