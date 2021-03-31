08b87696319702335b542e8f404ece75bdc9e22d
wy-luke <li1078797083@qq.com>
2021-03-31 20:52:24 +0800

    Add translation function

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -63,7 +68,7 @@ namespace CopyPlusPlus
                             continue;
                         }
                         text = text.Remove(counter + 1, 2);
-                    }               
+                    }
                 }
 
                 //string text_after = text.Replace("\n","").Replace("\r","");
@@ -110,9 +115,70 @@ namespace CopyPlusPlus
             MessageBox.Show("欢迎赞助我，加快开发进度！");
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Github_Click(object sender, RoutedEventArgs e)
         {
             Process.Start("explorer.exe", "https://github.com/CopyPlusPlus/CopyPlusPlus");
         }
+
+        private void BaiduTrans(object sender, RoutedEventArgs e)
+        {
+            // 原文
+            string q = "apple";
+            // 源语言
+            string from = "en";
+            // 目标语言
+            string to = "zh";
+            // 改成您的APP ID
+            string appId = API.baidu_id;
+            Random rd = new Random();
+            string salt = rd.Next(100000).ToString();
+            // 改成您的密钥
+            string secretKey = API.baidu_secretKey;
+            string sign = EncryptString(appId + q + salt + secretKey);
+            string url = "http://api.fanyi.baidu.com/api/trans/vip/translate?";
+            url += "q=" + HttpUtility.UrlEncode(q);
+            url += "&from=" + from;
+            url += "&to=" + to;
+            url += "&appid=" + appId;
+            url += "&salt=" + salt;
+            url += "&sign=" + sign;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+            request.UserAgent = null;
+            request.Timeout = 6000;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            //read json(retString) as a object
+            var result = JsonSerializer.Deserialize<Rootobject>(retString);
+            
+            Console.WriteLine(retString);
+            Console.WriteLine(result.trans_result[0].dst);
+
+        }
+        // 计算MD5值
+        public static string EncryptString(string str)
+        {
+            MD5 md5 = MD5.Create();
+            // 将字符串转换成字节数组
+            byte[] byteOld = Encoding.UTF8.GetBytes(str);
+            // 调用加密方法
+            byte[] byteNew = md5.ComputeHash(byteOld);
+            // 将加密结果转换为字符串
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in byteNew)
+            {
+                // 将字节转换成16进制表示的字符串，
+                sb.Append(b.ToString("x2"));
+            }
+            // 返回加密的字符串
+            return sb.ToString();
+        }
+
     }
 }
